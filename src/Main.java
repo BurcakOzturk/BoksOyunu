bf955e35f19a0d05dbfdb837975a9089b4cd1abd
Burçak ÖZTÜRK <159647466+BurcakOzturk@users.noreply.github.com>
2024-03-03 16:48:37 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-      Fighter marc = new Fighter("Marc" , 15 , 100, 90, 0);
-      Fighter alex = new Fighter("Alex" , 10 , 95, 100, 0);
+      Fighter marc = new Fighter("Marc" , 15 , 100, 90, 40);
+      Fighter alex = new Fighter("Alex" , 10 , 95, 100, 30);
       Ring r = new Ring(marc,alex , 90 , 100);
       r.run();
     }
